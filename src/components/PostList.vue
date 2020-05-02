6a65becb5807b3e7f91eb58c4fa6b2d3a391a871
Tab <d491405879@gmail.com>
2020-05-02 10:07:54 +0800

    安装axios，挂载到Vue实例中

---
+++ src/components/PostList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="loading" v-if="isLoading">
+      <img src="../assets/loading.gif" alt="loading">
+    </div>
+
+    <div>
+      <ul>
+        <li></li>
+      </ul>
+    </div>
+
+  </div>
+</template>
+
+<script lang="js">
+
+  export default {
+    name: "PostList",
+    data(){
+      return{
+        isLoading:false
+      }
+    },
+    methods:{
+      getData(){
+
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
